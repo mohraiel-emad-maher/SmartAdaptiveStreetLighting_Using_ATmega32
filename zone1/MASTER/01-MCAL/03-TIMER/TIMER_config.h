92b7c32c5a67f921e7b29d2565d7e06ec2469b9d
mohraiel-emad-maher <mohra.emad102@gmail.com>
2026-09-24 06:47:50 +0300

    Update TIMER_config.h

--- zone1/MASTER/01-MCAL/03-TIMER/TIMER_config.h
+++ zone1/MASTER/01-MCAL/03-TIMER/TIMER_config.h
@@ -1,11 +1,8 @@
 #ifndef TIMER_CONFIG_H
 #define TIMER_CONFIG_H
 
-#define 	  WAVEFORM_GEN_MODE          FAST_PWM_MODE
-#define 	  OC0_ACTION                 FAST_PWM_CLEAR_OC0_ON_COMPARE_MATCH__SET_OC0_ON_TOP
-
-/*  CLK_DIV_BY_8 for standard Timer operation, or external clock source for counters */
-#define 	 PRESCALER_TYPE     CLK_DIV_BY_8
-#define 	 PRESCALER_MASK     0b11111000
+#define TIMER0_MODE       TIMER0_PHASE_CORRECT
+#define TIMER0_OC0_MODE   TIMER0_NON_INVERTING
+#define TIMER0_PRESCALER  TIMER0_DIV_8
 
 #endif
